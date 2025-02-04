4fffd028b3344df8b57a9257194ab240007ac27a
Muhammad Rezky <mrezkysulihin@gmail.com>
2025-02-04 22:14:09 +0800

    feat: add negative emotions mock

--- Bako/Core/Constant/Mock/NegativeEmotions.swift
+++ Bako/Core/Constant/Mock/NegativeEmotions.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+let negativeEmotions: [EmotionModel] = [
+    EmotionModel(feel: "Exhausted", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Tired", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Overwhelmed", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Stressed", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Unmotivated", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Drained", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Frustrated", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Confused", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Distracted", iconType: .exhausted, category: .negative),
+    EmotionModel(feel: "Burned out", iconType: .exhausted, category: .negative),
+    
+    EmotionModel(feel: "Sad", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Lonely", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Hurt", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Anxious", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Worried", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Insecure", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Disappointed", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Guilty", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Heartbroken", iconType: .affectionate, category: .negative),
+    EmotionModel(feel: "Missing someone", iconType: .affectionate, category: .negative)
+]
